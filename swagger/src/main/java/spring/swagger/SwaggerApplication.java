0c4c595d01eb3aef5268220c2227926860de9aa7
lsw6684 <lsw6684@hanmail.net>
2023-11-09 11:49:33 +0900

    spring-doc

--- swagger/src/main/java/spring/swagger/SwaggerApplication.java
+++ swagger/src/main/java/spring/swagger/SwaggerApplication.java
@@ -2,8 +2,9 @@ package spring.swagger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= DataSourceAutoConfiguration.class)
 public class SwaggerApplication {
 
 	public static void main(String[] args) {
---
+++ swagger/src/main/java/spring/swagger/service/impl/FabricServiceImpl.java
@@ -0,0 +1,24 @@
+package spring.swagger.service.impl;
+
+import spring.swagger.model.Fabric;
+import spring.swagger.service.FabricService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FabricServiceImpl implements FabricService {
+
+    private static final Map<String, Fabric> fabricRepository = new HashMap<>();
+
+    // 샘플 데이터
+    static {
+        fabricRepository.put("12345", new Fabric("12345", "Cotton Blend"));
+        fabricRepository.put("67890", new Fabric("67890", "Polyester"));
+    }
+
+    @Override
+    public Fabric findDescriptionByFabricNo(String fabricNo) {
+        // fabric_no 코드로 description을 조회
+        return fabricRepository.get(fabricNo);
+    }
+}
